051a4dac4983d286ccd121da2af72db0cd48b8e0
Rajat <r_kumar1773346@fnashaweonline.ca>
2023-08-02 20:17:45 -0400

    Created Project

---
+++ iOSProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSProject2
+//
+//  Created by Rajat Kumar on 2023-08-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
